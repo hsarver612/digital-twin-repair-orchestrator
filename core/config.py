cf6b6a81eaf90a90d9284af679214bdaca96816b
harrison.sarver <113867131+hsarver612@users.noreply.github.com>
2026-02-04 12:25:26 -0600

    Add default configurations and catalogs to config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,15 @@
+DEFAULT_JURISDICTION = "IL"
+DEFAULT_FLEET_SIZE = 3000
 
+HORIZONS = [7, 14, 30, 60]
+WEATHER_LEVELS = ["normal", "rain", "snow", "hail"]
+
+PARTS_CATALOG = {
+    "front": ["bumper_cover", "headlamp", "hood", "grille", "adas_radar"],
+    "rear": ["bumper_cover", "tail_lamp", "trunk_lid", "rear_sensor"],
+    "side": ["door_shell", "mirror", "fender", "side_airbag"],
+    "glass": ["windshield", "rear_glass", "side_glass"],
+}
+
+SHOP_SPECIALTIES = ["general", "adas_calibration", "heavy_collision", "glass_only"]
+REGION_CLUSTERS = ["city", "inner_suburb", "outer_suburb", "rural"]
